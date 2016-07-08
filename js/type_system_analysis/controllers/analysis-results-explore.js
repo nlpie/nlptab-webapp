e59d725a453c8e7744ec9b8ed4455b8b1445a8d2
Ben Knoll <knol0061@umn.edu>
2016-07-08 13:23:42 -0500

    Export link from analysis results.

--- js/type_system_analysis/controllers/analysis-results-explore.js
+++ js/type_system_analysis/controllers/analysis-results-explore.js
@@ -1,8 +1,12 @@
 'use strict';
 
 angular.module('nlptabApp')
-  .controller('AnalysisResultsExploreCtrl', function ($q, $scope, $routeParams, matchCounts) {
+  .controller('AnalysisResultsExploreCtrl', function ($q, $scope, $routeParams, matchCounts, nlptabConfig, $http) {
     // Scope properties
     $scope.examplesOf = $routeParams.examplesOf || 'coOccurrences';
     $scope.matchCounts = matchCounts;
+
+    $scope.esServer = nlptabConfig.esServer;
+    $scope.analysisId = $routeParams.matchCountsId;
+    $scope.instance = nlptabConfig.instanceName;
   });
